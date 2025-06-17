cad5b6a1206404a808237739175b6d63e680e63c
Zurickata <rodrigoramirezcatrileo@gmail.com>
2025-06-16 22:11:29 -0400

    Dark Theme

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import sys
+import qdarkstyle
 from PyQt5.QtWidgets import QApplication
 from app.interface.main_window import MainWindow
 
 def main():
     app = QApplication(sys.argv)
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
     window = MainWindow()
     window.show()
     window.vtk_widget.Start()
